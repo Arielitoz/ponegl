711925a7b5dc2b0b537dba4a8ba27c86ff82b080
Arielitoz <arifrancisco78@gmail.com>
2023-11-06 15:02:51 -0300

    Testing threading & multiprocessing scan

--- networkMonitor.py
+++ networkMonitor.py
@@ -1,14 +1,19 @@
 import sys
 import socket
 from datetime import datetime
+import time
 import re
+import threading
+import multiprocessing
+
+# imports from pyfiglet import Figlet
 
 ip_add_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
 # Regular Expression Pattern to extract the number of ports you want to scan. 
 # You have to specify <lowest_port_number>-<highest_port_number> (ex 10-100)
 port_range_pattern = re.compile("([0-9]+)-([0-9]+)")
-# imports from pyfiglet import Figlet
-# write file ;; log history, and tcp dump
+
+# target - ip, port ranges / socket.AF_INET -> IPV4 family, SOCK_STREAM -> the socket type for TCP, the protocol that will be used to transport messages in the network.
 target = ""
 port_min = 0
 port_max = 65535
@@ -36,11 +41,14 @@ def scanTargetIp():
 
     try:
     # 65,535 existents ports / Scan every port on the target IP
-     for port in range(1,65535):
+     for port in range(port_min,port_max):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             socket.setdefaulttimeout(0.5)
             #Return open ports
             response = s.connect_ex((target, port))
+            # nwThread = threading.Thread(target=s.connect_ex((target, port)))
+            # nwThread.start()
+            # nwThread.join()
             if response == 0:
                 print("[*] Port {} is open".format(port))
                 s.close()
@@ -68,6 +76,9 @@ def scanRangedPorts():
             port_min = int(port_range_valid.group(1))
             port_max = int(port_range_valid.group(2))
             break
+    startTime = time.time()
+    print("\nScanning started at: " + str(datetime.now()))
+    print("\n")
     for port in range(port_min, port_max + 1):
         
         try:
@@ -89,6 +100,10 @@ def scanRangedPorts():
         for port in openPorts:
             # We use an f string to easily format the string with variables so we don't have to do concatenation.
             print(f"Port {port} is open on {target}.")
+
+    endTime = time.time()
+    processTime = (endTime - startTime) * 1000
+    print(f"\nprocess take {processTime:.2f} in ms.")
 
 def scanPort():
 
@@ -121,20 +136,3 @@ def validateUserOption():
         validateUserOption()
 
 validateUserOption()
-
-'''
-#startTime = ""
-endTime = ""
-totalEndtime = ""
- startTime = str(datetime.now())
-    print(startTime)
-
-    print("oi\n" * 10)
-    endTime = str(datetime.now())
-    print(endTime)
-
-    totalEndtime = int(startTime) - int(endTime)
-    print(totalEndtime)
-    https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers#Dynamic,_private_or_ephemeral_ports
-'''
-
--- steps.md
+++ steps.md
@@ -8,3 +8,6 @@
 - create process and  structure
 - write log file, history
 - add to databases
+
+## Other things
+- https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers#Dynamic,_private_or_ephemeral_ports
