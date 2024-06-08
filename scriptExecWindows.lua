cc965e6da7d5646f2be0c164a41e34c16623e60a
Arielitoz <arifrancisco78@gmail.com>
2024-06-08 18:15:46 -0300

    Update script - Loop while

--- scriptExecWindows.lua
+++ scriptExecWindows.lua
@@ -12,9 +12,11 @@
 #
 #
 #
- = [Anna, ariel, matheu]
 #
 #Declare variables
+
+$i=0
+
 #GivenName Primeiro Nome
 $Givenname = "João", "Maria", "Pedro", "Ana", "Paulo", "Carla", "Marcos", "Aline", "Lucas", "Fernanda", "Rafael", "Juliana", "Bruno", "Camila", "Eduardo", "Beatriz", "Rodrigo", "Tatiana", "Gustavo", "Patricia"
 #Surname Sobrenome
@@ -27,23 +29,10 @@ $Name = "João Silva", "Maria Oliveira", "Pedro Costa", "Ana Souza", "Paulo Pere
 $upname = "joao.silva", "maria.oliveira", "pedro.costa", "ana.souza", "paulo.pereira", "carla.santos", "marcos.rodrigues", "aline.almeida", "lucas.lima", " fernanda.gomes", "rafael.barbosa", "juliana.teixeira", "bruno.fernandes", "camila.martins", "eduardo.araujo", "beatriz.ribeiro", "rodrigo.carvalho", "tatiana.moreira", "gustavo.cardoso", "patricia.mendes"
 #SamAccountName
 $samAcc = "joao.silva", "maria.oliveira", "pedro.costa", "ana.souza", "paulo.pereira", "carla.santos", "marcos.rodrigues", "aline.almeida", "lucas.lima", " fernanda.gomes", "rafael.barbosa", "juliana.teixeira", "bruno.fernandes", "camila.martins", "eduardo.araujo", "beatriz.ribeiro", "rodrigo.carvalho", "tatiana.moreira", "gustavo.cardoso", "patricia.mendes"
-$Password = "SenhaForte123!", "SenhaForte456!", "SenhaForte789!", "SenhaForte101!", "SenhaForte202!", "SenhaForte303!", "SenhaForte404!", "SenhaForte505!", "SenhaForte606!", "SenhaForte707!", "SenhaForte808!", "SenhaForte909!", "SenhaForte111!", "SenhaForte222!", "SenhaForte333!", "SenhaForte444!", "SenhaForte555!", "SenhaForte666!", "SenhaForte777!", "SenhaForte888!"
+$pwd = "SenhaForte123!", "SenhaForte456!", "SenhaForte789!", "SenhaForte101!", "SenhaForte202!", "SenhaForte303!", "SenhaForte404!", "SenhaForte505!", "SenhaForte606!", "SenhaForte707!", "SenhaForte808!", "SenhaForte909!", "SenhaForte111!", "SenhaForte222!", "SenhaForte333!", "SenhaForte444!", "SenhaForte555!", "SenhaForte666!", "SenhaForte777!", "SenhaForte888!"
 
-#Create the user
-New-ADUser -Name $Name -GivenName $Givenname -Surname $Surname -DisplayName $Displayname -AccountPassword $SecureString
-
-#Set the password to never expire
-Set-ADUser -Identity $Identity -PasswordNeverExpires $true -ChangePasswordAtLogon $false -Enabled $true
-
-Dados de usuário - primeiroNome;ultimoNome;Iniciais;nomeCompleto;usuarioLogonName;senha;confirmaçãoSenha;
-
-
-Comando para criar usuários:
-
-New-ADUser -Name "Jason bone" -GivenName "Jason" -Surname "Boue" -UserPrincipalName "juse.burne" -SamAccountName
- "Jon.Bourne" -AccountPassword (ConvertTo-SecureString -AsPlainText "323212Rel" -Force) -DisplayName "Jason Bourne" -Ena
-bled $True
-
-New-ADUser -Name $Name -GivenName $GivenName -Surname $Surname -UserPrincipalName $upname -SamAccountName
- $samAcc -AccountPassword (ConvertTo-SecureString -AsPlainText $password -Force) -DisplayName $Displayname -Ena
-bled $True
+while($i -lt 20) {
+    New-ADUser -Name $Name[$i] -GivenName $GivenName[$i] -Surname $Surname[$i] -UserPrincipalName $upname[$i] -SamAccountName $samAcc[$i] -AccountPassword (ConvertTo-SecureString -AsPlainText $pwd[$i] -Force) -DisplayName $Displayname[$i] -Enabled $True
+    i++
+}
+# New-ADUser -Name $Name -GivenName $GivenName -Surname $Surname -UserPrincipalName $upname -SamAccountName $samAcc -AccountPassword (ConvertTo-SecureString -AsPlainText $password -Force) -DisplayName $Displayname -Enabled $True
